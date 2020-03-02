96cc8a669574f7d93d0358f2fb0fd4bfddb9f951
Ajit Goel <ajitgoel@gmail.com>
2020-03-01 23:48:40 -0600

    Build Vue.js Applications With TypeScript

---
+++ src/components/ToDoForm.ts
@@ -0,0 +1,15 @@
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import WithRender from './to-do-form.html';
+
+@WithRender
+@Component
+export default class ToDoForm extends Vue {
+
+  @Prop({ type: String, default: 'Add Task'})  readonly buttonText!: string
+
+  public task = '';
+  public emitTask(): void {
+    this.$emit('added', this.task);
+    this.task = '';
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDo />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ToDo from '@/components/ToDo.ts';
 
-export default {
-  name: 'Home',
+@Component({
   components: {
-    HelloWorld
-  }
-}
+    ToDo,
+  },
+})
+export default class Home extends Vue {}
 </script>
